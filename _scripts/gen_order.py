f6e16aa6e3ac346b4947f5a5e7ee16d7f989a590
Bhuvan Venkatesh <bhuvan.venkatesh21@gmail.com>
2018-11-10 23:21:48 -0600

    Added a bit of documentation

--- _scripts/gen_order.py
+++ _scripts/gen_order.py
@@ -1,16 +1,26 @@
+"""
+This file generates another file
+"""
 import argparse
 import yaml
 
+help_text = """
+Converts a order.yaml file into a order.tex files that has a series of includes in the specified order.
+The output file is stdout, meaning you should use redirection to save it to a particular file name.
+"""
+
 def main(args):
     file_name = args.name
+    # Yaml load will load lists in order by default
     reorder = yaml.load(open(file_name, 'r'))
     templ = '\include{{{}}}'
+
     for file in reorder:
         render = templ.format(file)
         print(render)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('name')
+    parser = argparse.ArgumentParser(help=help_text)
+    parser.add_argument('name', help="Name of the order file", type=str)
     args = parser.parse_args()
     main(args)
--- _scripts/push_to_wiki.sh
+++ _scripts/push_to_wiki.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
+# Every script should have thisj
 set +e
-if [ -z ${1+x} ]; then
+
+# see if token is passed in through the command line and set
+if [ -z ${1+DOES_NOT_EXIST} ]; then
     echo "TOKEN UNSET";
     exit 1;
 fi
+
+export DOCS_SHA=$(git rev-parse --short HEAD)
+
+# Create a temp directory, so we don't get raced by the filesystem
 WIKI_DIR=`mktemp -d`
 
-echo "Cloning"
-git clone https://github.com/${TRAVIS_REPO_SLUG}.wiki.git ${WIKI_DIR}
+CLONE_URL=`https://github.com/${TRAVIS_REPO_SLUG}.wiki.git`
+echo "Cloning $CLONE_URL into $WIKI_DIR"
+git clone $CLONE_URL ${WIKI_DIR}
+
+# If we get race condition on read only files, we need to fix our build system
+# no temp directory here
 
 echo "Generating Wiki"
 python3 _scripts/gen_wiki.py order.yaml ${WIKI_DIR}
-SAVE=$1
+
+
 cd ${WIKI_DIR}
+
 git config credential.helper "store --file=.git/credentials"
-echo "https://bhuvy2:${SAVE}@github.com" > .git/credentials 2>/dev/null
+echo "https://bhuvy2:${1}@github.com" > .git/credentials 2>/dev/null
 git config --global user.email "bhuvan.venkatesh21@gmail.com"
 git config --global user.name "Bhuvan Venkatesh"
 
 git add -A
-git commit -m "Updating wiki"
+git commit -m "Updating wiki to ${DOCS_SHA}"
 git push origin
 
 cd ${TRAVIS_BUILD_DIR}
