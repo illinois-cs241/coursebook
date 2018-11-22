2bb713a37e9f59c675f1fe705e302c708c12e2c8
Bhuvan Venkatesh <bhuvan.venkatesh21@gmail.com>
2018-11-21 20:06:37 -0600

    Safety plus specify before

--- _scripts/push_to_wiki.sh
+++ _scripts/push_to_wiki.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Every script should have thisj
-set +e
+set -e
 
 DOCS_SHA=$(git rev-parse --short HEAD)
 
@@ -10,6 +10,7 @@ WIKI_DIR=`mktemp -d`
 
 CLONE_URL="git@github.com:${TRAVIS_REPO_SLUG}.wiki.git"
 echo "Cloning $CLONE_URL into $WIKI_DIR"
+git config --global core.sshCommand "ssh -i /tmp/deploy_wiki -F /dev/null"
 git clone $CLONE_URL $WIKI_DIR
 
 # If we get race condition on read only files, we need to fix our build system
@@ -22,7 +23,6 @@ cd ${WIKI_DIR}
 
 git add -A
 git commit -m "Updating wiki to ${DOCS_SHA}"
-git config core.sshCommand "ssh -i /tmp/deploy_wiki -F /dev/null"
 git push origin
 
 cd ${TRAVIS_BUILD_DIR}
--- _scripts/site_deploy.sh
+++ _scripts/site_deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e;
+
+git config --global core.sshCommand "ssh -i /tmp/deploy_site -F /dev/null"
 git clone --recurse-submodules -j8 -b develop --depth 1 git@github.com:illinois-cs241/illinois-cs241.github.io.git ${CLONE_DIR}
 cd ${CLONE_DIR}
 git checkout develop
@@ -9,7 +12,6 @@ export DOCS_SHA=$(git rev-parse --short HEAD)
 cd ..
 git add _docs
 git commit -m "Updating docs to ${DOCS_SHA}"
-git config core.sshCommand "ssh -i /tmp/deploy_site -F /dev/null"
 git push origin develop
 
 cd ${TRAVIS_BUILD_DIR}
