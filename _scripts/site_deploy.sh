7caf47a53d2fc519aa19c65037570808578a3e6e
Bhuvan Venkatesh <bhuvan.venkatesh21@gmail.com>
2018-12-16 10:57:08 -0600

    Update site_deploy.sh

--- _scripts/site_deploy.sh
+++ _scripts/site_deploy.sh
@@ -3,7 +3,7 @@
 set -e;
 
 git config --global core.sshCommand "ssh -i /tmp/deploy_site -F /dev/null"
-git clone j8 -b develop --depth 1 git@github.com:illinois-cs241/illinois-cs241.github.io.git ${CLONE_DIR}
+git clone -b develop --depth 200 git@github.com:illinois-cs241/illinois-cs241.github.io.git ${CLONE_DIR}
 git submodule update --depth 200
 git submodule sync
 pushd ${CLONE_DIR}
