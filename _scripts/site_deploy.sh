91515ad8415ab0887d95b177f6592d6619437793
Bhuvan Venkatesh <bhuvan.venkatesh21@gmail.com>
2018-12-16 11:03:38 -0600

    Update site_deploy.sh

--- _scripts/site_deploy.sh
+++ _scripts/site_deploy.sh
@@ -6,14 +6,14 @@ git config --global core.sshCommand "ssh -i /tmp/deploy_site -F /dev/null"
 git clone -b develop --depth 200 git@github.com:illinois-cs241/illinois-cs241.github.io.git ${CLONE_DIR}
 git submodule update --depth 200
 git submodule sync
-pushd ${CLONE_DIR}
+cd ${CLONE_DIR}
 git checkout develop
-pushd _wikibook_project/
+cd _wikibook_project/
 git pull origin master
 export DOCS_SHA=$(git rev-parse --short HEAD)
-popd
+cd ..
 git add _wikibook_project/
 git commit -m "Updating docs to ${DOCS_SHA}" || true
 git push origin develop
 
-popd
+cd ${TRAVIS_BUILD_DIR}
