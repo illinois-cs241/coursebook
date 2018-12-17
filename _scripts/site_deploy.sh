e49908dfd6468038e4906d69192fda0459529155
bhuvy <bhuvan.venkatesh21@gmail.com>
2018-12-17 14:33:16 -0600

    sdf

--- _scripts/site_deploy.sh
+++ _scripts/site_deploy.sh
@@ -4,9 +4,11 @@ set -e;
 set -x;
 git config --global core.sshCommand "ssh -i /tmp/deploy_site -F /dev/null"
 git clone -b develop --depth 1 git@github.com:illinois-cs241/illinois-cs241.github.io.git ${CLONE_DIR}
-git submodule update --init --recursive --depth 200
-git submodule sync
 cd ${CLONE_DIR}
+git submodule update --init --recursive
+git submodule update --recursive --depth 200
+
+ls _wikibook_project/
 git checkout develop
 cd _wikibook_project/
 echo $(pwd)
