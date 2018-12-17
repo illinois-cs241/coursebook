5205b41cdc3c224c7ee914af912c677fcb539c46
bhuvy <bhuvan.venkatesh21@gmail.com>
2018-12-17 14:14:02 -0600

    Debugging

--- _scripts/site_deploy.sh
+++ _scripts/site_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e;
-
+set -x;
 git config --global core.sshCommand "ssh -i /tmp/deploy_site -F /dev/null"
 git clone -b develop --depth 200 git@github.com:illinois-cs241/illinois-cs241.github.io.git ${CLONE_DIR}
 git submodule update --depth 200
@@ -9,6 +9,7 @@ git submodule sync
 cd ${CLONE_DIR}
 git checkout develop
 cd _wikibook_project/
+echo $(pwd)
 git pull origin master
 export DOCS_SHA=$(git rev-parse --short HEAD)
 cd ..
