d71dbcff8c730f985b1da7a0e6c27bdfb23a8f72
bhuvy <bhuvan.venkatesh21@gmail.com>
2018-12-17 15:13:21 -0600

    Trying out parallel builds

---
+++ _scripts/deploy.sh
@@ -0,0 +1,15 @@
+if test $BUILD_FOCUS = WIKI; then
+    export NUM_RETRIES=3
+    export BUILD_TIME=10
+    export CLONE_DIR=`mktemp -d`
+    bash _scripts/push_to_wiki.sh;
+else
+    TMP_FILE=`mktemp`;
+    cp main.pdf $TMP_FILE;
+    git config --global core.sshCommand "ssh -i /tmp/deploy_pdf -F /dev/null";
+    git checkout -b pdf_deploy;
+    cp $TMP_FILE coursebook.pdf;
+    git add coursebook.pdf;
+    git commit -m "Adding coursebook";
+    git push origin pdf_deploy;
+fi;
---
+++ _scripts/script.sh
@@ -0,0 +1,5 @@
+if test BUILD_FOCUS = "WIKI"; then
+    echo "Dummy Step";
+else
+    latexmk;
+fi;
