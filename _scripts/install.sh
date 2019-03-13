51ebdfbf4831249bd3f34e77de066d3cbfe22959
bhuvy <bhuvan.venkatesh21@gmail.com>
2019-03-12 20:02:37 -0500

    Debugging info

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -o errexit
+
 if test $BUILD_FOCUS = "WIKI"
 then
     pushd ~
     wget https://github.com/jgm/pandoc/releases/download/2.7/pandoc-2.7-1-amd64.deb
+    ls
     sudo dpkg -i pandoc-2.7.1-amd64.deb
     popd
     pip install -r requirements.txt
