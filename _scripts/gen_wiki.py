bea02394abc1f819e8860a8d0ce6d55a6b869329
bhuvy <bhuvan.venkatesh21@gmail.com>
2019-03-12 19:27:08 -0500

    Fixing latex chars

--- _scripts/gen_wiki.py
+++ _scripts/gen_wiki.py
@@ -99,7 +99,7 @@ def main(args):
         if sys_ret != 0:
             raise OSError("Cat command for {} failed".format(files_m))
         md_path = files_m.md_path
-        command = 'pandoc --toc --self-contained -f latex -t markdown_github -s --filter _scripts/pandoc_wiki_filter.py {} -o {} '.format(tex_tmp_path, md_path)
+        command = 'pandoc --toc --self-contained -f latex+tex_math_dollars -t markdown_github -s --filter _scripts/pandoc_wiki_filter.py {} -o {} '.format(tex_tmp_path, md_path)
         print(command)
         sys_ret = os.system(command)
         if sys_ret != 0:
