8e666d652cde4a46fc4eb6b37724c97d92c90969
bhuvy <bhuvan.venkatesh21@gmail.com>
2019-03-12 20:04:35 -0500

    ugh

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -6,8 +6,7 @@ if test $BUILD_FOCUS = "WIKI"
 then
     pushd ~
     wget https://github.com/jgm/pandoc/releases/download/2.7/pandoc-2.7-1-amd64.deb
-    ls
-    sudo dpkg -i pandoc-2.7.1-amd64.deb
+    sudo dpkg -i pandoc-2.7-1-amd64.deb
     popd
     pip install -r requirements.txt
 else
