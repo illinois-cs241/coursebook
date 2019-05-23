8d6ca1a92d7103c7eb6125bf464fe6013e2a8df7
bhuvy <bhuvan.venkatesh21@gmail.com>
2019-05-23 12:10:46 -0500

    More hotfixes

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,6 +2,9 @@
 
 set -o errexit
 
+# Install packages that we need for python. Python3.6 is already installed
+pip install -r requirements.txt
+
 if test $BUILD_FOCUS = "WIKI"
 then
     # Install pandoc from source because Ubuntu is high outdated
@@ -9,10 +12,7 @@ then
     wget https://github.com/jgm/pandoc/releases/download/2.7/pandoc-2.7-1-amd64.deb
     sudo dpkg -i pandoc-2.7-1-amd64.deb
     popd
-
-    # Install packages that we need for python. Python3.6 is already installed
-    pip install -r requirements.txt
 else
-    echo "Dummy step"
+    echo ""
 fi;
 
