a51c5966d812b49850dd0aceadc391b9b4ac8a09
bhuvy <bhuvan.venkatesh21@gmail.com>
2019-07-23 11:00:57 -0500

    Revised install

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -5,15 +5,12 @@ set -o errexit
 # Install packages that we need for python. Python3.6 is already installed
 pip install -r requirements.txt
 
-if test $BUILD_FOCUS = "WIKI"
+if test $BUILD_FOCUS = "WIKI" || test $BUILD_FOCUS = "EPUB"
 then
     # Install pandoc from source because Ubuntu is high outdated
     pushd ~
     wget https://github.com/jgm/pandoc/releases/download/2.7/pandoc-2.7-1-amd64.deb
     sudo dpkg -i pandoc-2.7-1-amd64.deb
     popd
-else
-    # EPUB and MD
-    echo ""
 fi;
 
