4e6f2e198c6ee5add59b9acf60cf91dba0166d79
bhuvy <bhuvan.venkatesh21@gmail.com>
2019-07-23 11:23:11 -0500

    Fixed bad build caching

--- _scripts/script.sh
+++ _scripts/script.sh
@@ -7,8 +7,9 @@ then
     echo "Dummy Step"
 elif test $BUILD_FOCUS = "EPUB"
 then
-    pandoc --toc -s -f latex -t epub --filter pandoc-citeproc --filter _scripts/pandoc_epub_filter.py -M link-citations=true --epub-cover-image _images/cover.png -M author="B. Venkatesh, L. Angrave, et Al." -o main.epub main.tex
-    ls -la main.epub
+    make order.tex;
+    pandoc --toc -s -f latex -t epub --filter pandoc-citeproc --filter _scripts/pandoc_epub_filter.py -M link-citations=true --epub-cover-image _images/cover.png -M author="B. Venkatesh, L. Angrave, et Al." -o main.epub main.tex;
+    ls -la main.epub;
 else
     make
 fi;
