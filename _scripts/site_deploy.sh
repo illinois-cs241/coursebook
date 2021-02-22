51aeabe3c71d314c943479aef2d6949307821488
Eric Zhang <ezhang887@gmail.com>
2021-02-22 11:23:15 -0800

    Enable github-actions on this branch for testing

--- _scripts/site_deploy.sh
+++ _scripts/site_deploy.sh
@@ -7,8 +7,8 @@ git config --global core.sshCommand "ssh -i /tmp/deploy_site -F /dev/null"
 export DOCS_SHA=$(git rev-parse --short HEAD)
 
 # Clone with submodules
-#git clone -b develop --depth 1 git@github.com:illinois-cs241/illinois-cs241.github.io.git ${CLONE_DIR}
-#cd ${CLONE_DIR}
+git clone -b develop --depth 1 git@github.com:illinois-cs241/illinois-cs241.github.io.git ${CLONE_DIR}
+cd ${CLONE_DIR}
 
 #git commit --allow-empty -m "Updating docs to ${DOCS_SHA}"
 #git push origin develop
